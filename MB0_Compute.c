42c098d63cc649e261ab9f489303392f90f1a66a
lauleehong <lauleehong@u.nus.edu.sg>
2017-03-22 23:07:01 +0800

    inital skeleton for microblaze_0

---
+++ MB0_Compute.c
@@ -0,0 +1,435 @@
+/*
+-- --------------------------------------------------------------------------
+-- Author         : Lee-Hong Lau and Justin Yeo
+-- Date           : 22/03/2017
+-- Version        : V1.0
+-- License        : MIT
+-----------------------------------------------------------------------------
+*/
+#include "sys/init.h"
+#include "xmk.h"
+#include "xmbox.h"
+#include "xmutex.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <xparameters.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
+#include <sys/timer.h>
+
+#define RECT_WIDTH 	     40
+#define RECT_LENGTH      240
+#define RECT_GAP 	       10
+#define RECT_COLOR 	     0x000000ff
+#define X1 		 	         50
+#define X2 			         X1+RECT_WIDTH+RECT_GAP
+#define X3 			         X2+RECT_WIDTH+RECT_GAP
+#define X4 			         X3+RECT_WIDTH+RECT_GAP
+#define Y1 			         50
+#define BALL_X 		       10
+#define BALL_Y 		       10
+#define BALL_DIR 	       180
+#define BALL_COLOR 	     0x0000ff00
+
+#define MSG_COLUMN	     1
+#define MSG_BALL	       2
+
+// mailbox declaration
+#define MY_CPU_ID 			  XPAR_CPU_ID
+#define MBOX_DEVICE_ID		XPAR_MBOX_0_DEVICE_ID
+static XMbox Mbox;	/* Instance of the Mailbox driver */
+
+// MUTEX ID PARAMETER for HW Mutex
+#define MUTEX_DEVICE_ID 	XPAR_MUTEX_0_IF_1_DEVICE_ID
+#define MUTEX_NUM 			  0
+XMutex Mutex;
+
+struct msg {
+  int id,msg_type,x,y,w,l,d;
+  uint32_t c;
+};
+
+typedef struct {
+  int dir,x,y;
+  uint32_t col;
+} ball_msg;
+
+pthread_attr_t attr;
+struct sched_param sched_par;
+pthread_t mailbox_controller, ball, col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, bar, scoreboard;
+volatile int taskrunning;
+
+//---------------------------------------
+
+void send (int id, int msg_type, int x, int y, int w, int l, int d, uint32_t c) {
+
+  struct msg send_msg;
+  int msgid;
+
+  send_msg.id = id;
+  send_msg.msg_type = msg_type;
+  send_msg.x = x;
+  send_msg.y = y;
+  send_msg.l = l;
+  send_msg.d = d;
+  send_msg.c = c;
+
+  //ENTER YOUR CODE HERE TO SEND DATA TO THREAD DISPLAY
+  XMutex_Lock(&Mutex, MUTEX_NUM);
+  if (msg_type == MSG_COLUMN) {
+    xil_printf("PRODCON: Producer -- Start Column %d!\r\n", id);
+  } else {
+    xil_printf("PRODCON: Producer -- Start Ball!\r\n");
+  }
+  XMutex_Unlock(&Mutex, MUTEX_NUM);
+
+  msgid = msgget (id, IPC_CREAT ) ;
+  if( msgid == -1 ) {
+    xil_printf ("PRODCON: Producer -- ERROR while opening Message Queue. Errno: %d\r\n", errno) ;
+    pthread_exit (&errno);
+  }
+  if( msgsnd (msgid, &send_msg, 32, 0) < 0 ) { // blocking send
+    xil_printf ("PRODCON: Producer -- msgsnd of message(%d) ran into ERROR. Errno: %d. Halting..\r\n", errno);
+    pthread_exit(&errno);
+  }
+
+  XMutex_Lock(&Mutex, MUTEX_NUM);
+  print("PRODCON: Producer done !\r\n");
+  XMutex_Unlock(&Mutex, MUTEX_NUM);
+
+  pthread_exit(0);
+}
+
+//---------------------------------------
+
+static void MailboxExample_Receive(XMbox *MboxInstancePtr, ball_msg *ball_pointer) {
+  XMbox_ReadBlocking(MboxInstancePtr,	ball_pointer, 16);
+  xil_printf("Display received : Ball, x = %d  y= %d  d= %d  c=%08x \r\n", ball_pointer->x, ball_pointer->y, ball_pointer->dir, ball_pointer->col);
+}
+
+
+void* thread_mb_controller () {
+  while(1) {
+    // do something
+  }
+}
+
+void* thread_ball () {
+  while(1) {
+    // do something
+  }
+}
+
+void* thread_brick_col_1 () {
+  while(1) {
+    // do something
+  }
+}
+
+void* thread_brick_col_2 () {
+  while(1) {
+    // do something
+  }
+}
+
+void* thread_brick_col_3 () {
+  while(1) {
+    // do something
+  }
+}
+
+void* thread_brick_col_4 () {
+  while(1) {
+    // do something
+  }
+}
+
+void* thread_brick_col_5 () {
+  while(1) {
+    // do something
+  }
+}
+
+void* thread_brick_col_6 () {
+  while(1) {
+    // do something
+  }
+}
+
+void* thread_brick_col_7 () {
+  while(1) {
+    // do something
+  }
+}
+
+void* thread_brick_col_8 () {
+  while(1) {
+    // do something
+  }
+}
+
+void* thread_brick_col_9 () {
+  while(1) {
+    // do something
+  }
+}
+
+void* thread_brick_col_10 () {
+  while(1) {
+    // do something
+  }
+}
+
+void* thread_bar () {
+  while(1) {
+    // do something
+  }
+}
+
+void* thread_scoreboard () {
+  while(1) {
+    // do something
+  }
+}
+
+/** 
+  *  Main - Inititialization for HW Mutex, Mailbox and Threads
+  */
+
+int main_prog(void) {   // This thread is statically created (as configured in the kernel configuration) and has priority 0 (This is the highest possible)
+
+  int ret;
+  int Status;
+  XMutex_Config *MutexConfigPtr;
+  XMbox_Config *ConfigPtr;
+
+  // Init SW Mutex
+  ret = pthread_mutex_init (&uart_mutex, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) init uart_mutex...\r\n", ret);
+  }
+  print("--Initialized SW Mutex-- uB0 \r\n");
+
+  // Init HW Mutex
+  MutexConfigPtr = XMutex_LookupConfig(MUTEX_DEVICE_ID);
+  if (MutexConfigPtr == (XMutex_Config *)NULL){
+    ("B1-- ERROR  init HW mutex...\r\n");
+  }
+  Status = XMutex_CfgInitialize(&Mutex, MutexConfigPtr, MutexConfigPtr->BaseAddress);
+  if (Status != XST_SUCCESS){
+    xil_printf ("B1-- ERROR  init HW mutex...\r\n");
+  }
+
+  print("-- Entering main_prog() uB0 RECEIVER--\r\n");
+
+
+  // Configure and init mailbox
+  ConfigPtr = XMbox_LookupConfig(MBOX_DEVICE_ID );
+  if (ConfigPtr == (XMbox_Config *)NULL) {
+    print("-- Error configuring Mbox uB0 receiver--\r\n");
+    return XST_FAILURE;
+  }
+
+  Status = XMbox_CfgInitialize(&Mbox, ConfigPtr, ConfigPtr->BaseAddress);
+  if (Status != XST_SUCCESS) {
+    print("-- Error initializing Mbox uB0 receiver--\r\n");
+    return XST_FAILURE;
+  }
+
+
+  /** Initialize Threads
+    * -----------------------------------------------------------------------------------
+    * thread_mb_controller  (highest priority)  : Mailbox Controller to pipe data to MB1  
+    * thread_ball                               : Ball                                    
+    * thread_brick_col_1 ~ thread_brick_col_10  : Brick Columns
+    * thread_bar                                : Bar
+    * thread_scoreboard     (lowest priority)   : Scoreboard
+    * -----------------------------------------------------------------------------------
+    */
+  pthread_attr_init (&attr);
+
+  // Priority 1 for thread_mb_controller
+  sched_par.sched_priority = 1;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread_mb_controller
+  ret = pthread_create (&mailbox_controller, &attr, (void*)thread_mb_controller, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_mb_controller...\r\n", ret);
+  }
+  else {
+    xil_printf ("Mailbox Controller Thread launched with ID %d \r\n",mailbox_controller);
+  }
+
+  // Priority 2 for thread_ball
+ sched_par.sched_priority = 2;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread_ball
+  ret = pthread_create (&ball, &attr, (void*)thread_ball, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_ball...\r\n", ret);
+  }
+  else {
+    xil_printf ("Ball Thread launched with ID %d \r\n",ball);
+  }
+
+  // Priority 3 for thread_brick_col_1
+  sched_par.sched_priority = 3;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread_brick_col_1
+  ret = pthread_create (&col1, &attr, (void*)thread_brick_col_1, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_brick_col_1...\r\n", ret);
+  }
+  else {
+    xil_printf ("Brick Column 1 Thread launched with ID %d \r\n",col1);
+  }
+
+  // Priority 4 for thread_brick_col_2
+  sched_par.sched_priority = 4;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread_brick_col_2
+  ret = pthread_create (&col2, &attr, (void*)thread_brick_col_2, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_brick_col_2...\r\n", ret);
+  }
+  else {
+    xil_printf ("Brick Column 2 Thread launched with ID %d \r\n",col2);
+  }
+
+  // Priority 5 for thread_brick_col_3
+  sched_par.sched_priority = 5;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread 5
+  ret = pthread_create (&col3, &attr, (void*)thread_brick_col_3, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_brick_col_3...\r\n", ret);
+  }
+  else {
+    xil_printf ("Brick Column 3 Thread launched with ID %d \r\n",col3);
+  }
+
+  // Priority 6 for thread_brick_col_4
+  sched_par.sched_priority = 6;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread_brick_col_4
+  ret = pthread_create (&col4, &attr, (void*)thread_brick_col_4, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_brick_col_4...\r\n", ret);
+  }
+  else {
+    xil_printf ("Brick Column 4 launched with ID %d \r\n",col4);
+  }
+
+  // Priority 7 for thread_brick_col_5
+  sched_par.sched_priority = 7;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread_brick_col_5
+  ret = pthread_create (&col5, &attr, (void*)thread_brick_col_5, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_brick_col_5...\r\n", ret);
+  }
+  else {
+    xil_printf ("Brick Column 5 launched with ID %d \r\n",col5);
+  }
+
+  // Priority 8 for thread_brick_col_6
+  sched_par.sched_priority = 8;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread_brick_col_6
+  ret = pthread_create (&col6, &attr, (void*)thread_brick_col_6, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_brick_col_6...\r\n", ret);
+  }
+  else {
+    xil_printf ("Brick Column 6 launched with ID %d \r\n",col6);
+  }
+
+  // Priority 9 for thread_brick_col_7
+  sched_par.sched_priority = 9;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread_brick_col_7
+  ret = pthread_create (&col7, &attr, (void*)thread_brick_col_7, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_brick_col_7...\r\n", ret);
+  }
+  else {
+    xil_printf ("Brick Column 7 launched with ID %d \r\n",col7);
+  }
+
+  // Priority 10 for thread_brick_col_8
+  sched_par.sched_priority = 10;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread_brick_col_8
+  ret = pthread_create (&col8, &attr, (void*)thread_brick_col_8, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_brick_col_8...\r\n", ret);
+  }
+  else {
+    xil_printf ("Brick Column 8 launched with ID %d \r\n",col8);
+  }
+
+  // Priority 11 for thread_brick_col_9
+  sched_par.sched_priority = 11;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread_brick_col_9
+  ret = pthread_create (&col9, &attr, (void*)thread_brick_col_9, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_brick_col_9...\r\n", ret);
+  }
+  else {
+    xil_printf ("Brick Column 9 launched with ID %d \r\n",col9);
+  }
+
+    // Priority 12 for thread_brick_col_10
+  sched_par.sched_priority = 12;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread_brick_col_10
+  ret = pthread_create (&col10, &attr, (void*)thread_brick_col_10, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_brick_col_10...\r\n", ret);
+  }
+  else {
+    xil_printf ("Brick Column 10 launched with ID %d \r\n",col10);
+  }
+
+    // Priority 13 for thread_bar
+  sched_par.sched_priority = 13;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread_bar
+  ret = pthread_create (&bar, &attr, (void*)thread_bar, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_bar...\r\n", ret);
+  }
+  else {
+    xil_printf ("Bar Thread launched with ID %d \r\n",bar);
+  }
+
+    // Priority 14 for thread_scoreboard
+  sched_par.sched_priority = 14;
+  pthread_attr_setschedparam(&attr,&sched_par);
+  //start thread_scoreboard
+  ret = pthread_create (&scoreboard, &attr, (void*)thread_scoreboard, NULL);
+  if (ret != 0) {
+    xil_printf ("-- ERROR (%d) launching thread_scoreboard...\r\n", ret);
+  }
+  else {
+    xil_printf ("Scoreboard Thread launched with ID %d \r\n",scoreboard);
+  }
+
+  return 0;
+}
+
+int main (void) {
+  print("-- Entering main() uB0 RECEIVER--\r\n");
+  xilkernel_init();
+  xmk_add_static_thread(main_prog,0);
+  xilkernel_start();
+  //Start Xilkernel
+  xilkernel_main ();
+
+  //Control does not reach here
+  return 0;
+}
