aa67018535b6b4d12aa3296cc72e92ebcf63a8a3
Justin Yeo <Justin Yeo>
2017-03-29 09:54:37 +0800

    Uploaded mid-project working code with folder structure.

---
+++ MultiProcessor.sdk/proj0/Debug/src/platform.d
@@ -0,0 +1,25 @@
+src/platform.d: ../src/platform.c \
+ ../../xilkernel_bsp_0/microblaze_0/include/xparameters.h \
+ ../../xilkernel_bsp_0/microblaze_0/include/xil_cache.h \
+ ../../xilkernel_bsp_0/microblaze_0/include/mb_interface.h \
+ ../../xilkernel_bsp_0/microblaze_0/include/xil_types.h \
+ ../../xilkernel_bsp_0/microblaze_0/include/xil_assert.h \
+ ../../xilkernel_bsp_0/microblaze_0/include/xil_exception.h \
+ ../../xilkernel_bsp_0/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../xilkernel_bsp_0/microblaze_0/include/xparameters.h:
+
+../../xilkernel_bsp_0/microblaze_0/include/xil_cache.h:
+
+../../xilkernel_bsp_0/microblaze_0/include/mb_interface.h:
+
+../../xilkernel_bsp_0/microblaze_0/include/xil_types.h:
+
+../../xilkernel_bsp_0/microblaze_0/include/xil_assert.h:
+
+../../xilkernel_bsp_0/microblaze_0/include/xil_exception.h:
+
+../../xilkernel_bsp_0/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
